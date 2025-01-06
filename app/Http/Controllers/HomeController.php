de9bee2bc572ee9d147f3e5e8818776372de967d
wisnu IDS <wisnu.bhakti@i-saku.com>
2025-01-06 10:13:38 +0700

    [Bhakti] Menu

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -61,4 +61,75 @@ class HomeController extends Controller
         ]);
     }
 
+    public function menu($id, Request $request)
+    {
+        $client = new Client();
+        $dataKategori = [];
+        $dataproduk = [];
+
+        $reset = $request->query('reset');
+        if ($reset === 'Y') {
+            session()->forget('cart'); //hapus keranjang
+        }
+
+        // Ambil data cart dari session
+        $cart = session()->get('cart', []);
+        $cartCount = count($cart);
+
+        // produk di keranjang dan merchantId berbeda
+        if (!empty($cart)) {
+            $currentMerchantId = reset($cart)['merchantId'];
+            if ($currentMerchantId !== $id) {
+                return back()->with([
+                    'duplicate' => 'Keranjang Anda berisi produk dari resto lain. Keranjang akan dihapus jika Anda melanjutkan.',
+                    'merchantNew' => $id,
+                ]);
+            }
+        }
+
+        try {
+            // Hit API Kategori
+            $responseKategori = $client->request('GET', 'https://api.klajek.com/api/category/' . $id);
+            $dataKategori = json_decode($responseKategori->getBody()->getContents(), true);
+        } catch (\Exception $e) {
+            Log::error('Gagal memuat data kategori: ' . $e->getMessage());
+        }
+
+        try {
+            // Hit API Produk
+            $url = 'https://api.klajek.com/api/menus/' . $id;
+            $responseProduk = $client->request('GET', $url);
+            $dataproduk = json_decode($responseProduk->getBody()->getContents(), true);
+
+            $kategori = $request->query('kategori');
+            //dd($kategori);
+            if (!empty($kategori)) {
+                if($kategori == "all"){
+                    $dataproduk = $dataproduk;
+                }else{
+                    $dataproduk['data'] = array_filter($dataproduk['data'], function ($item) use ($kategori) {
+                        return isset($item['kategori']['kategori']) && $item['kategori']['kategori'] === $kategori;
+                    });
+                }
+            }else{
+                $dataproduk = $dataproduk;
+            }
+
+        } catch (\Exception $e) {
+            Log::error('Gagal memuat data produk: ' . $e->getMessage());
+        }
+
+        // Hit API Merchant
+        $responseMerchant = $client->request('GET', 'https://api.klajek.com/api/merchants');
+        $dataMerchant = json_decode($responseMerchant->getBody()->getContents(), true);
+        $merchant = collect($dataMerchant['data'])->firstWhere('id', $id);
+
+        return view('home-page/restoran', [
+            'kategori' => $dataKategori, 
+            'produk' => $dataproduk,
+            'merchant' => $merchant,
+            'cartCount' => $cartCount
+        ]);
+    }
+
 }
